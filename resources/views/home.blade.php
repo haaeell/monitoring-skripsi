c4604e39b8a1dc39f7fc53e37d3caf5a68e20180
haaeell <haikaljawahir434@gmail.com>
2024-07-01 00:22:26 +0700

    slicing dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,37 @@
 
 @section('content')
     <div class=" mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card shadow">
                     <div class="card-header">{{ __('Dashboard') }}</div>
 
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+                     <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sequi est numquam error quaerat consectetur reprehenderit eos dolores vitae architecto corporis quis maxime veritatis
+                     </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                        {{ __('You are logged in!') }}
+                    <div class="card-body">
+                     <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sequi est numquam error quaerat consectetur reprehenderit eos dolores vitae architecto corporis quis maxime veritatis
+                     </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                     <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sequi est numquam error quaerat consectetur reprehenderit eos dolores vitae architecto corporis quis maxime veritatis
+                     </p>
                     </div>
                 </div>
             </div>
