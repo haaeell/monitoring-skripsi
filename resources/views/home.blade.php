1442223618816827aa8f20698360a2da8acbd00a
haaeell <haikaljawahir434@gmail.com>
2024-07-01 22:47:22 +0700

    fix: dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,33 +5,22 @@
         <div class="row">
             <div class="col-md-4">
                 <div class="card shadow">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">Data Dosen</div>
 
                     <div class="card-body">
                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sequi est numquam error quaerat consectetur reprehenderit eos dolores vitae architecto corporis quis maxime veritatis
+                        {{$jumlahDosen}}
                      </p>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card shadow">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">Data Mahasiswa</div>
 
                     <div class="card-body">
                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sequi est numquam error quaerat consectetur reprehenderit eos dolores vitae architecto corporis quis maxime veritatis
-                     </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card shadow">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod sequi est numquam error quaerat consectetur reprehenderit eos dolores vitae architecto corporis quis maxime veritatis
+                        {{$jumlahMahasiswa}}
                      </p>
                     </div>
                 </div>
